21aed5fa8d52b5190e61690982da651b665bcb0d
Vivan <vivanpamta221@gmail.com>
2025-10-05 23:45:49 +0530

    Delete Backend_Experiment-17-18 directory

--- Backend_Experiment-17-18/account-transfer-system/models/Account.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const accountSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    balance: { type: Number, required: true, default: 0 }
-});
-
-export const Account = mongoose.model('Account', accountSchema);
-
