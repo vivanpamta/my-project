0d4c32655f9596438914c121ecf6cff32201749e
Vivan <vivanpamta221@gmail.com>
2025-10-05 23:45:19 +0530

    Delete Backend-Basic directory

--- Backend-Basic/Experiment-16/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-// Middleware to log HTTP method, URL, and timestamp
-const logger = (req, res, next) => {
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] ${req.method} ${req.url}`);
-    next();
-};
-
-module.exports = logger;
-
